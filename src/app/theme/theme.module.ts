b1eb1788ace66df59e4442d2ac3601de114d2e5c
aniket17 <anidandawate1@gmail.com>
2020-09-16 05:28:59 +0530

    Added brand

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { BrandComponent } from './brand/brand.component';
 import { BaseComponent } from './base/base.component';
-
 @NgModule({
-  declarations: [BaseComponent],
-  imports: [CommonModule],
+  declarations: [BaseComponent, BrandComponent],
+  imports: [CommonModule, RouterModule, MatIconModule],
   exports: [BaseComponent],
 })
 export class ThemeModule {}
