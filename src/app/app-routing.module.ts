d400513386473c0d68d5ff0986014544e028cfc6
aniket17 <anidandawate1@gmail.com>
2020-09-16 04:59:27 +0530

    Added Theme module with Base component. Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './theme/base/base.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    children: [], //lazy loaded modules
+  },
+  //login and other error pages
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
