33db138178e4c4831f16db4ca86030193170e5cf
aniket17 <anidandawate1@gmail.com>
2020-09-16 07:54:24 +0530

    implemented movie service

--- src/app/core/_services/movie.service.ts
+++ src/app/core/_services/movie.service.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Movie } from '..';
+import { PageResultsModel } from '../_models/page-results.model';
 
 @Injectable()
 export class MovieService {
@@ -10,4 +14,20 @@ export class MovieService {
   BASE_URL = environment.apiServer;
   MOVIES_URL = `${this.BASE_URL}/movies`;
   constructor(private http: HttpClient) {}
+
+  find(params: PageResultsModel): Observable<PageResultsModel> {
+    return this.http
+      .post<PageResultsModel>(`${this.MOVIES_URL}/find`, params)
+      .pipe(catchError((err) => this.handleError(err)));
+  }
+  getMovie(id) {
+    return this.http
+      .get<Movie>(`${this.MOVIES_URL}/${id}`)
+      .pipe(catchError((err) => this.handleError(err)));
+  }
+  handleError(err: HttpErrorResponse) {
+    //do something with error.. show a toast TODO
+    console.error(err);
+    return of(null);
+  }
 }
