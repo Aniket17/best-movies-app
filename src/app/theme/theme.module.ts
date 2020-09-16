eb3f4abdacb9dcf01f078d042fa981deea4de86b
aniket17 <anidandawate1@gmail.com>
2020-09-16 05:32:05 +0530

    Added app header

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -5,8 +5,14 @@ import { RouterModule } from '@angular/router';
 import { BrandComponent } from './brand/brand.component';
 import { BaseComponent } from './base/base.component';
 import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
-  declarations: [BaseComponent, BrandComponent, MobileHeaderComponent],
+  declarations: [
+    BaseComponent,
+    BrandComponent,
+    MobileHeaderComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule, RouterModule, MatIconModule],
   exports: [BaseComponent],
 })
